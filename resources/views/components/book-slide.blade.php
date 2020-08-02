7acb702f6a8582fe0e04a6f13616bb2210f12e61
ReinanHS <reinangabriel1520@gmail.com>
2020-08-02 19:30:39 -0300

    Melhorias no card versão 1

--- resources/views/components/book-slide.blade.php
+++ resources/views/components/book-slide.blade.php
@@ -11,30 +11,51 @@
             </div>
             <div class="col-12">
                 <div class="row">
-                    @for ($i = 0; $i < (12/2); $i++)
-                    <div class="col-sm-12 col-md-4 col-lg-2">
-                        <div class="card">
-                            <div class="card-capa d-flex justify-content-center">
-                                <div class="card-img-effect"></div>
-                                <div class="card-img-load card-load-item">
-                                    <img class="card-img-top" src="https://livrariaconcreta.com.br/wp-content/uploads/2017/01/livro-vermelho_andrew-lang_CAPA_FINAL_CURVAS-01.jpg" alt="...">
+                    @for ($i = 0; $i < 10; $i++)
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-container">
+                            <div class="card-top">
+                              <img src="{{ url('img/livros/carousel/a-garota-do-lago.png') }}">
+                              <div class="card-top-icons">
+                                <div class="card-icon-love">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                    </svg>
                                 </div>
+                                <div class="card-icon-cart">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-basket-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3z"/>
+                                    </svg>
+                                </div>
+                                <div class="card-icon-share">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-share-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M12.024 3.797L4.499 7.56l-.448-.895 7.525-3.762.448.894zm-.448 9.3L4.051 9.335 4.5 8.44l7.525 3.763-.448.894z"/>
+                                        <path fill-rule="evenodd" d="M13.5 5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm0 11a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm-11-5.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+                                    </svg>
+                                </div>
+                              </div>
                             </div>
                             <div class="card-body">
-                                <a href="#" class="card-title">
-                                    <div class="title-limit">
-                                        Mitologia Nórdica
+                                <a class="card-title" href="#">
+                                    <div class="title" title="A Garota do Lago - Livro 1">
+                                        <p>A Garota do Lago - Livro 1</p>
                                     </div>
                                 </a>
-                                <a href="#" class="card-subtitle mb-2 text-muted">
-                                    <div class="subtitle-limit">
-                                        Neil Gaiman
+                                <a class="card-author" href="#">
+                                    <div class="author" title="Author Marcio Ardenghe D. Peres">
+                                        <span>Marcio Ardenghe D. Peres</span>
                                     </div>
                                 </a>
-                                <div class="d-flex align-items-end">
-                                    <div class="stars" style="background: rgba(0, 0, 0, 0) linear-gradient(to right, #737373 89.6%, rgb(204, 204, 204) 70%) repeat scroll 0% 0% padding-box text;" aria-label="Rating of this product is 2.3 out of 5."></div>
-                                    <a href="#" class="card-link">R$ 12,80</a>
+                            </div>
+                            <div class="card-button">
+                              <div class="rating-section">
+                                <div class="stars-rating">
+                                    <div class="stars"></div>
                                 </div>
+                                <div class="card-price">
+                                  <span>R$ 70,99</span>
+                                </div>
+                              </div>
                             </div>
                         </div>
                     </div>
