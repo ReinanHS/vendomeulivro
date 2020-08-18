ec92c541a34e9cad2ed0c6c9974a722b1dfbc456
ReinanHS <reinangabriel1520@gmail.com>
2020-08-17 22:42:30 -0300

    Criando as rotas do menu

---
+++ app/Http/Controllers/Store/MainController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function ofertas(){
+        print('ofertas');
+    }
+
+    public function lancamentos(){
+        print('lancamentos');
+    }
+
+    public function historico(){
+        print('Historico');
+    }
+
+    public function lojasOficiais(){
+        print('oi');
+    }
+
+    public function vender(){
+        print('oi');
+    }
+
+    public function categorias(){
+        print('oi');
+    }
+
+    public function atendimento(){
+        print('Atendimento ao Cliente');
+    }
+}
